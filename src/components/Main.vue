e4087b25c285d58da9c7f8b09eb861c5b85371bb
Reda Nabil <reda.nabil@smarterplan.io>
2022-01-14 11:53:30 +0100

    mainContent

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <section class="category"></section>
+    <section class="mainContent"></section>
+    <section class="chatList"></section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  components: {},
+};
+</script>
+
+<style></style>
