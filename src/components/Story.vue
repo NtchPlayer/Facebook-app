a2ffd8787ad3355348e446c722b5e653415e484a
IcePlayer <nathanchevalet@orange.fr>
2022-01-14 15:24:01 +0100

    Create Story container

---
+++ src/components/Story.vue
@@ -0,0 +1,48 @@
+<template>
+  <section id="container-story">
+    <ItemStory
+        v-for="(story, i) in storyData"
+        :key="i"
+        :profile-photo="story.profilePhoto"
+        :img="story.img"
+        :title="story.title"
+    />
+  </section>
+</template>
+
+<script>
+import ItemStory from './item/ItemStory.vue'
+
+export default {
+  name: 'Story',
+  components: {
+    ItemStory
+  },
+  data () {
+    return {
+      storyData: [
+        {
+          title: 'Manu Romero Villareal',
+          img: 'https://picsum.photos/200/300',
+        }, {
+          title: 'World Of Clarence',
+          img: 'https://picsum.photos/200/300',
+        },  {
+          title: 'Vincent Lahouze',
+          img: 'https://picsum.photos/200/300',
+        },  {
+          title: 'Paul Silve',
+          img: 'https://picsum.photos/200/300',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#container-story{
+  justify-content: space-between;
+  display: flex;
+}
+</style>
